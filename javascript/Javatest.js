1dd7d07877be79c7475c0964ab9032fb375ef392
MatteoGunetto <Matteogunetto@tiscali.it>
2023-04-26 15:14:24 +0200

    add index

--- javascript/Javatest.js
+++ javascript/Javatest.js
@@ -1,4 +1,21 @@
 // Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferitoInfine scrivi sulla pagina nomecognomecolorepreferito21 
+let nomeUtente, cognomeUtente, colorePreferito, password;
+
+nomeUtente = prompt("inserisci nome")
+console.log(nomeUtente);
+
+cognomeUtente = prompt("inserisci cognome")
+console.log(cognomeUtente);
+
+colorePreferito = prompt("inserisci colore preferito")
+console.log(colorePreferito);
+
+
+password =nomeUtente + cognomeUtente + colorePreferito + 21
+
+console.log(password);
+
+document.getElementById('password').innerHTML = password; 
 
 
 
